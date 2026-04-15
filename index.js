996ae790f2bf850035c183b1f147d18628714303
Martin Waraho Mundia <martinwaraho2@gmail.com>
2026-04-15 10:20:16 +0300

    Add combineUsers function to merge username arrays with merge date

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+require('datejs');
 
+function combineUsers(...args) {
+
+  const combinedObject = {
+    users: []
+  };
+
+  args.forEach(arr => {
+    combinedObject.users.push(...arr);
+  });
+
+  const today = new Date();
+  combinedObject.merge_date = today.toString("M/d/yyyy");
+
+  return combinedObject;
+}
 
 
 module.exports = {
